b91f6a722d24c6e54794a08c468bc013fd644275
Egon Pasztor <pasztor@gmail.com>
2018-03-02 23:25:25 -0800

    !

--- gldemo/src/com/generic/base/Mesh2.java
+++ gldemo/src/com/generic/base/Mesh2.java
@@ -109,8 +109,8 @@ public class Mesh2 {
       return faceToDirectedEdge[face];
    }
    // Given a boundaryEdge-id, you can retrieve the id of the directedEdge mapped to it:
-   public int boundaryEdgeToDirectedEdge (int boundaryEdge) {
-      return boundaryEdgeToDirectedEdge[boundaryEdge];
+   public int boundaryToDirectedEdge (int boundaryEdge) {
+      return boundaryToDirectedEdge[boundaryEdge];
    }   
 
    // -----------------------------------------------------   
@@ -128,7 +128,43 @@ public class Mesh2 {
    // Call this to connect vertices to form a new Face:
    public int addFace(int... vertices) {
       
-      
+
+      // --------------------------------------------
+      // adding a triangular face 
+      //    will either add 3 boundary edges,  (adding a new face in empty space)
+      //             or add 1 boundary edge,   (if 1 side is connected to an existing face)
+      //          or remove 1 boundary edge,   (if 2 sides are connected to existing faces)
+      //          or remove 3 boundary edges,  (filling a triangular hole in an existing mesh)
+      //
+      //    it will either add 3 new edges     (adding a new face in empty space)
+      //                or add 2 new edges     (if 1 side is connected to an existing face)
+      //                or add 1 new edges     (if 2 sides is connected to an existing face)
+      //               or add no new edges     (filling a triangular hole in an existing mesh)
+      //
+      // if 3 new edges are created, that's 6 new directedEdges --
+      //          3 of which point to the "new face"
+      //      and 3 of which have "boundary edge ids"
+      // 
+      // if 2 new edges are created, (the triangle has 1 side adjacent to an existing face)
+      //      then 1 existing directedEdge (which has a boundary-id) changes to a face id.
+      //          (that makes a boundary-edge-id "unused" -- 
+      //          and boundaryEdgeToDirectedEdge[ boundary_edge_id ] should be cleared,
+      //              (set to -1?), it should no longer point back to the directedEdge got changed.
+      //
+      //      creating 2 new edges means increasing
+      //      the directedEdgeData array by (4 *  sizeOfDirectedEdgeData),
+      //      that is, creating 4 new directedEdges which must be "wired in to existing directedEdges.
+      //         (the "inner two" directedEdges connect to the "existing" directedEdge that
+      //             was "transformed" in the previous step)
+      //          the "outer two" directedEdges are connected to the boundary-loop that was
+      //             left broken by the removal of the "existing" directedEdge 
+      //             was "transformed" in the previous step)
+      //
+      //
+      //      the "outer" two directedEdges both need "boundary-edge-ids"..
+      //
+      //      one of the "new" boundary-edges can get the same id as the recently deleted one..
+      //      
       return -1;
    }
 
@@ -189,52 +225,151 @@ public class Mesh2 {
    
    private int[] vertexToDirectedEdge;
    private int[] faceToDirectedEdge;
-   private int[] boundaryEdgeToDirectedEdge;
-   
-   private static final int sizeOfDirectedEdgeData = 4;
+   private int[] boundaryToDirectedEdge;   
    private int[] directedEdgeData;
    
-   // --------------------------------------------
-   // adding a triangular face 
-   //    will either add 3 boundary edges,  (adding a new face in empty space)
-   //             or add 1 boundary edge,   (if 1 side is connected to an existing face)
-   //          or remove 1 boundary edge,   (if 2 sides are connected to existing faces)
-   //          or remove 3 boundary edges,  (filling a triangular hole in an existing mesh)
-   //
-   //    it will either add 3 new edges     (adding a new face in empty space)
-   //                or add 2 new edges     (if 1 side is connected to an existing face)
-   //                or add 1 new edges     (if 2 sides is connected to an existing face)
-   //               or add no new edges     (filling a triangular hole in an existing mesh)
-   //
-   // if 3 new edges are created, that's 6 new directedEdges --
-   //          3 of which point to the "new face"
-   //      and 3 of which have "boundary edge ids"
-   // 
-   // if 2 new edges are created, (the triangle has 1 side adjacent to an existing face)
-   //      then 1 existing directedEdge (which has a boundary-id) changes to a face id.
-   //          (that makes a boundary-edge-id "unused" -- 
-   //          and boundaryEdgeToDirectedEdge[ boundary_edge_id ] should be cleared,
-   //              (set to -1?), it should no longer point back to the directedEdge got changed.
-   //
-   //      creating 2 new edges means increasing
-   //      the directedEdgeData array by (4 *  sizeOfDirectedEdgeData),
-   //      that is, creating 4 new directedEdges which must be "wired in to existing directedEdges.
-   //         (the "inner two" directedEdges connect to the "existing" directedEdge that
-   //             was "transformed" in the previous step)
-   //          the "outer two" directedEdges are connected to the boundary-loop that was
-   //             left broken by the removal of the "existing" directedEdge 
-   //             was "transformed" in the previous step)
-   //
-   //
-   //      the "outer" two directedEdges both need "boundary-edge-ids"..
-   //
-   //      one of the "new" boundary-edges can get the same id as the recently deleted one..
-   //      
    
-   enum Layer { VERTEX, EDGE, FACE, DIRECTED_EDGE, BOUNDARY_EDGE };
+   public static class PrimitiveIntArray {
+      public PrimitiveIntArray () {
+         array = new int[10];
+         size = 0;
+      }
+      
+      public int size()     { return size;  }
+      public int[] array()  { return array; }
+      
+      public void setSize(int newSize) {
+         if (array.length < newSize) {
+            int newCapacity = array.length;
+            while (newCapacity < newSize) newCapacity *= 2;
+            int[] newArray = new int [newCapacity];
+            System.arraycopy(array, 0, newArray, 0, size);
+            array = newArray;
+         }
+         size = newSize;
+      }
+      
+      private int[] array;
+      private int size;
+   }
+
+   private PrimitiveIntArray vertexToDirectedEdge_;
+   private PrimitiveIntArray faceToDirectedEdge_;
+   private PrimitiveIntArray boundaryToDirectedEdge_;
+
+   private static final int sizeOfDirectedEdgeData = 4;
+   private PrimitiveIntArray directedEdgeData_;
    
    
-   // so when we need a new BoundaryEdge id, what do we do?
+   // -------------------------------------------------------
+   // So, we need separate arrays keeping track of the "deleted ids"
+   // among the vertex/face/edge/boundary lists.
+   // -------------------------------------------------------
+   private PrimitiveIntArray deletedEdgeIndices;
+   private PrimitiveIntArray deletedVertexIndices;
+   private PrimitiveIntArray deletedFaceIndices;
+   private PrimitiveIntArray deletedBoundaryIndices;
+   
+
+   
+   // -------------------------------------------------------
+   // Now then, the "userdata" segment...
+   // -------------------------------------------------------
+   
+   enum Layer         { VERTEX, EDGE, FACE, BOUNDARY };
+   enum PrimitiveType { INTEGER, FLOAT, OBJECT }; 
+   
+   public static class DataLayerType {
+      public final Layer layer;
+      public final int elementCount;
+      public final PrimitiveType elementType;
+      
+      public DataLayerType(Layer layer, int elementCount, PrimitiveType elementType) {
+         this.layer = layer;
+         this.elementCount = elementCount;
+         this.elementType = elementType;
+      }
+   }
+   
+   // imagine that the user wants to add a structure with 3-floats-per-vertex...
+   // then they call:
+   //
+   // DataLayer addDataLayer(String name, DataLayerType type);
+   //
+   // and we could have "getDataLayer(String name)" and "removeDataLayer(String name)" as well...
+   //
+   // but what's a DataLayer?
+   //
+   //    well, it knows its name, and it knows the Mesh it's part of...
+   //    and for a given elementID, it should provide "read-write" access to an "elementCount" x "elementType" region of data...
+   //
+   // IF elementType is INTEGER, the DataLayer holds a PrimitiveIntArray
+   //    whose size is pegged to elementCount x mesh.numVertices(),
+   //    but, come on ... 
+   
+   public interface ElementData {
+      public int size();
+   }
+   public interface ElementIntData extends ElementData {
+      public int get(int i);
+   }
+   public interface MutableElementIntData extends ElementIntData {
+      public void set(int i, int v);
+   }
+   public interface ElementFloatData extends ElementData {
+      public float get(int i);
+   }
+   public interface MutableElementFloatData extends ElementFloatData {
+      public void set(int i, float v);
+   }
+   
+   
+   public static abstract class DataLayer {
+      public final Mesh2 mesh;
+      public final String name;
+      public final DataLayerType type;
+      
+      private DataLayer(Mesh2 mesh, String name, DataLayerType type) {
+         this.mesh = mesh;
+         this.name = name;
+         this.type = type;
+      }
+      public int size() {
+         switch (type.layer) {
+            case VERTEX   : return mesh.numVertices();
+            case EDGE     : return mesh.numEdges();
+            case FACE     : return mesh.numFaces();
+            case BOUNDARY : return mesh.numBoundaryEdges();
+         }
+         throw new RuntimeException();
+      }
+      public abstract ElementData at(int i);
+   }
+   
+   public static abstract class IntDataLayer extends DataLayer {
+      private IntDataLayer(Mesh2 mesh, String name, DataLayerType type) {
+         super (mesh, name, type);
+         if (type.elementType != PrimitiveType.INTEGER) {
+            throw new RuntimeException();
+         }
+      }
+      
+      public MutableElementIntData at(int i) {
+         final int offset = i * type.elementCount;
+         return new MutableElementIntData() {
+            public int get(int i) {
+               return 0;
+            }
+            public int size() {
+               return 0;
+            }
+            public void set(int i, int v) {
+            }
+         };
+      }
+   }
+   
+   
    
    
    public static class BlockArray {
@@ -250,6 +385,35 @@ public class Mesh2 {
       private int[] data;  // data.length = elementSize * currentCapacity;
       private int[] deletedIndices;
    }
+
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
    
    
 }
